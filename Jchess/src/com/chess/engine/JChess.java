e8eb8915a6cfa8c7cd2f47a018493d1c22ad48c6
Md. Rifat Farazi <61563706+justrifat@users.noreply.github.com>
2022-09-18 19:11:54 +0600

    Update JChess.java

--- Jchess/src/com/chess/engine/JChess.java
+++ Jchess/src/com/chess/engine/JChess.java
@@ -5,7 +5,7 @@ import com.chess.engine.board.Board;
 public class JChess {
     public  static void main(String[] args){
         Board board = Board.createStandardBoard();
-        System.out.println(board);;
+        System.out.println(board);
 
     }
 }
